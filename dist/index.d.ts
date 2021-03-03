3b1a6d974ddb176f32a607cef2d77035d9226ec2
Jiseop Han <jshan2017@gmail.com>
2021-03-04 00:25:06 +0900

    remove dist from git

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare type UseMaruReturn<T> = [T, (value: T) => void];
-export declare const useMaru: <T>(key: string, initialValue?: T | undefined) => UseMaruReturn<T>;
-export {};
--- dist/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type UseMaruReturn<T> = [T, (value: T) => void];
-export declare const useMaruInitialization: (initialData: Record<string, any>) => void;
-export declare const useMaru: <T>(key: string) => UseMaruReturn<T>;
-export declare const clearStore: () => void;
-export {};
